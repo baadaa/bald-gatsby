196e4daef6d638e3cd6f515de66a6d0b4021e40e
Bumhan Yu <bumhan.yu@gmail.com>
2021-08-13 01:13:59 -0400

    generate portfolio pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -34,11 +34,59 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     }
   `);
 
+  const portfolioQuery = await graphql(`
+    query {
+      allMdx(
+        sort: { fields: frontmatter___order, order: ASC }
+        filter: { frontmatter: { type: { eq: "work" } } }
+      ) {
+        edges {
+          node {
+            id
+            frontmatter {
+              title
+              order
+              category
+              slug
+            }
+          }
+          next {
+            frontmatter {
+              title
+              slug
+            }
+          }
+          previous {
+            frontmatter {
+              title
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if (blogQuery.errors || portfolioQuery.errors) {
+    reporter.panicOnBuild('🚨  ERROR: Loading "createPages" query');
+  }
+
   const blogPosts = blogQuery.data.allMdx.edges;
+  const workPosts = portfolioQuery.data.allMdx.edges;
   blogPosts.forEach(({ node, next, previous }) => {
     createPage({
       path: node.frontmatter.slug,
-      component: path.resolve('./src/components/BlogPostLayout.tsx'),
+      component: path.resolve('./src/components/Blog/BlogPostLayout.tsx'),
+      context: { id: node.id, next, previous },
+    });
+  });
+
+  workPosts.forEach(({ node, next, previous }) => {
+    createPage({
+      path: node.frontmatter.slug,
+      component: path.resolve(
+        './src/components/Portfolio/PortfolioPostLayout.tsx'
+      ),
       context: { id: node.id, next, previous },
     });
   });
