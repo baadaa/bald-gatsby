b8a364411444fe5c7cef33d438df87bea2bad098
Bumhan Yu <bumhan.yu@gmail.com>
2021-08-27 15:23:54 -0400

    add konami code intro

--- src/components/SiteInfo.tsx
+++ src/components/SiteInfo.tsx
@@ -31,6 +31,7 @@ const InfoWrapperStyles = styled.div`
     position: relative;
     border: 1px solid #575757;
     color: #ddd;
+    text-align: center;
     .chrome {
       position: absolute;
       padding: 0.75rem;
@@ -91,7 +92,6 @@ const InfoWrapperStyles = styled.div`
       }
     }
     h5 {
-      text-align: center;
       font-size: 2.4rem;
       margin: 1.5rem 0 0;
       font-weight: 400;
@@ -104,11 +104,36 @@ const InfoWrapperStyles = styled.div`
     p {
       font-family: 'Work sans', sans-serif;
       font-size: 1.2rem;
-      text-align: center;
       margin: 0;
     }
     a {
       color: inherit;
+    }
+    hr {
+      border-bottom: 1px solid #3a3a3a;
+      background-color: #3a3a3a;
+      margin: 2rem 0;
+    }
+    h6 {
+      font-size: 1.3rem;
+      font-weight: 600;
+      text-align: center;
+    }
+    kbd {
+      display: inline-block;
+      font-weight: 600;
+      line-height: 1;
+      font-size: 1rem;
+      padding: 0.3em;
+      border-radius: 0.2em;
+      font-family: 'SFMono-Regular', Consolas, 'Roboto Mono', 'Droid Sans Mono',
+        'Liberation Mono', Menlo, Courier, monospace;
+      background-color: var(--yellow800);
+      border: 1px solid var(--yellow700);
+      color: var(--yellow300);
+    }
+    kbd + kbd {
+      margin-left: 0.3rem;
     }
   }
 `;
@@ -141,6 +166,21 @@ const SiteInfo: React.FC<SiteInfoProps> = ({ isVisible, cb }) => (
       <p style={{ marginTop: '2rem' }}>
         &copy; {new Date().getFullYear()} by Bumhan Yu
       </p>
+      <hr />
+      <h6>Available Konami codes</h6>
+      <kbd>s</kbd>
+      <kbd>h</kbd>
+      <kbd>a</kbd>
+      <kbd>k</kbd>
+      <kbd>e</kbd> • <kbd>b</kbd>
+      <kbd>l</kbd>
+      <kbd>u</kbd>
+      <kbd>r</kbd> • <kbd>r</kbd>
+      <kbd>e</kbd>
+      <kbd>s</kbd>
+      <kbd>e</kbd>
+      <kbd>t</kbd>
+      <br />
     </div>
   </InfoWrapperStyles>
 );
